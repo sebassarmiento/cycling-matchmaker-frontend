177112a26b930ffc35353d6d18c252d94e78e01a
Miguel Jara <migueljar20@gmail.com>
2024-02-28 00:57:14 -0500

    Add Login Support

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,43 +1,99 @@
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import "../styles/login.css"
-import { useState } from "react";
+import { useState, useContext } from "react";
 import LoaderWheel from "../components/LoaderWheel";
+import { gql, useMutation } from '@apollo/client';
+import { AuthContext } from "../context/auth";
 
 const LoginPage = () => {
 
     const navigate = useNavigate();
 
+    const context = useContext(AuthContext);
+
     const [userName, setUserName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const [loginSuccessful, setLoginSuccessful] = useState<boolean>(true); // true for prototype only
+    const [loginSuccessful, setLoginSuccessful] = useState<boolean>(false); // true for prototype only
 
-    const [loading, setLoading] = useState<boolean>(false);
+    //const [loading, setLoading] = useState<boolean>(false);
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUserName(e.target.value)
+        const updatedUsername = e.target.value;
+        setValues((prevValues) => ({
+        ...prevValues,
+        username: updatedUsername,
+        }));
+        setUserName(e.target.value);
     }
 
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
+        const updatedPassword = e.target.value;
+        setValues((prevValues) => ({
+        ...prevValues,
+        password: updatedPassword,
+        }));
+        setPassword(e.target.value);
     }
 
+    const [errors, setErrors] = useState({});
+
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const [values, setValues] = useState({
+        username: "",
+        password: "",
+        remember: "false",
+      });
+
+    const [loginUser, { loading }] = useMutation(LOGIN_USER,  {
+        update(_, { data: { login: userData } }) {
+            context.login(userData);
+            handleLogin
+            if(userData) {
+                setLoginSuccessful(true);
+            }
+        },
+        onCompleted(data) {
+            console.log(data)
+            if(data) {
+                setLoginSuccessful(true);
+            }
+        },
+        onError(err) {
+            console.log(values)
+            console.error("GraphQL Mutation Error:", err);
+            console.log("GraphQL Errors:", err.graphQLErrors);
+            setErrors(err.graphQLErrors);
+            const errorObject = (err.graphQLErrors[0] as any)?.extensions?.exception?.errors
+            const errorMessage = Object.values(errorObject).flat().join(', ');
+            setErrorMessage(errorMessage);
+                },
+    
+        variables: values,
+        });
+
+        
+    function loginUserCallback() {
+        loginUser();
+    }
     const handleLogin = (e: any) => {
+        console.log("Variables before mutation:", values);
         if(userName == "" || password == ""){
-            console.log("hereeeeee")
             return null;
         } else {
             e.preventDefault();
             console.log("The name is " + userName)
             // fetch
+            loginUserCallback();
             if(loginSuccessful){
-                setLoading(true);
+                //setLoading(true);
                 setTimeout(() => {
                     // navigate to "/app/profile", aka login
                     navigate("/app/profile");
                     console.log("navigated!");
-                }, 3000);
+                }, 400);
             }
         }
     }
@@ -58,6 +114,11 @@ const LoginPage = () => {
                     <Link to="/" >Cycling matchmaker</Link>
                 </h1>
 
+                {errorMessage !== "" && 
+                <div className="signup-form-input" >
+                    <label>Incorrect Username or Password</label>
+                </div>}
+
                 <div className="login-form-input" >
                     <label htmlFor="username" >Username or email</label>
                     <input id="username" onChange={handleNameChange} type="text" value={userName} />
@@ -77,4 +138,22 @@ const LoginPage = () => {
     )
 };
 
+const LOGIN_USER = gql`
+mutation login(
+    $username: String!
+    $password: String!
+    $remember: String!
+  ) {
+    login(
+      loginInput: {
+        password: $password
+        username: $username
+        remember: $remember
+      }
+    ) {
+      username
+      loginToken
+    }
+  }
+`;
 export default LoginPage;
